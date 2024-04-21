b5f9771040cd3e9f20c5b161ffe0a28503e0797a
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-04-21 10:23:44 +0800

    Added new module - base.

---
+++ app/base/__init__.py
@@ -0,0 +1,55 @@
+from flask import Blueprint
+from sqlalchemy import event
+
+
+def create_blueprint() -> Blueprint:
+    from . import admin, middleware, models, views
+
+    blueprint = Blueprint('base', __name__, url_prefix='/',
+            static_folder=None, template_folder=None)
+    
+    blueprint.admin_views = ()
+    
+    # blueprint.add_url_rule('/', view_func=views.index)
+    # blueprint.add_url_rule('/task/sleep/', view_func=views.start_task)
+    # blueprint.add_url_rule('/task/<id>/', view_func=views.get_task)
+
+    return blueprint
+
+def prepare_blueprint():
+    from flask import current_app as app
+
+    from ..extensions import (
+        db,
+        security,
+    )
+    from .models import (
+        Group,
+        GroupAccess,
+        AccessNode,
+        Role,
+        User,
+        UserAccess,
+    )
+
+    try:
+        models = ()
+        
+        if not all(db.engine.dialect.has_table(db.session.connection(),
+                model.__table__.name, model.__table__.schema) for model in models):
+            return None
+        
+        if (base_access := AccessNode.get_by_full_name('base')) is None:
+            base_access = AccessNode.create_by_full_name('base')
+
+        # Create permission_node for models.
+        for model in models:
+            if (model_pn_name := getattr(model, 'access_node_full_name', None)) \
+                    and AccessNode.get_by_full_name(model_pn_name) is None:
+                AccessNode.create_by_full_name(model_pn_name)
+
+        db.session.commit()
+    
+    except Exception:
+        db.session.rollback()
+        raise
---
+++ app/base/models.py
@@ -0,0 +1,168 @@
+from datetime import (
+    date,
+    datetime,
+)
+from typing import (
+    List,
+    Optional,
+)
+
+from sqlalchemy import (
+    Boolean,
+    Date,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+    UniqueConstraint,
+)
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column,
+    relationship,
+)
+from sqlalchemy_utils import (
+    ScalarListType,
+    URLType,
+)
+
+from ..auth.models import (
+    GranularAccessMixin,
+    User,
+    WriterMixin,
+)
+from ..core.database import (
+    DateTimeTrackerMixin,
+    UuidMixin,
+)
+from ..extensions import db
+
+
+class BaseModel(
+    db.Model,
+    UuidMixin,
+    DateTimeTrackerMixin,
+    WriterMixin,
+    GranularAccessMixin,
+):
+
+    __abstract__ = True
+
+
+class Person(BaseModel):
+
+    __tablename__ = 'people'
+    __table_args__ = (
+        UniqueConstraint(
+            'first_name',
+            'last_name',
+            'postfix',
+            name='uix_full_name',
+        ),
+    )
+    access_node_full_name = f"base.{__tablename__}"
+
+    prefix: Mapped[Optional[str]] = mapped_column(String(10))
+    first_name: Mapped[str] = mapped_column(String(255))
+    middle_name: Mapped[Optional[str]] = mapped_column(String(255))
+    last_name: Mapped[str] = mapped_column(String(255))
+    postfix: Mapped[Optional[str]] = mapped_column(String(10))
+    nickname: Mapped[Optional[str]] = mapped_column(String(50))
+    birthday: Mapped[Optional[date]] = mapped_column(Date)
+    user_id: Mapped[Optional[int]] = mapped_column(Integer,
+            ForeignKey('users.id'), unique=True)
+
+    user: Mapped[User] = relationship(foreign_keys=[user_id])
+
+    def __repr__(self) -> str:
+        return self.full_name
+    
+    @hybrid_property
+    def full_name(self) -> str:
+        full_name = self.first_name
+
+        if self.prefix:
+            full_name = self.prefix + ' ' + full_name
+
+        if self.middle_name:
+            full_name = full_name + ' ' + self.middle_name
+        
+        full_name = full_name + ' ' + self.last_name
+
+        if self.postfix:
+            full_name = full_name + ' ' + self.postfix
+
+        return full_name
+    
+    @hybrid_property
+    def age(self) -> Optional[int]:
+        if self.birthday is None:
+            return None
+        
+        birthdate = self.birthday
+        today = datetime.utcnow().date()
+        age = today.year - birthdate.year - ((today.month, today.day)
+                < (birthdate.month, birthdate.day))
+
+        return age
+
+
+class SitePage(BaseModel):
+
+    __tablename__ = 'pages'
+    access_node_full_name = f"base.{__tablename__}"
+
+    url_title: Mapped[str] = mapped_column(String(255), unique=True)
+    title: Mapped[str] = mapped_column(String(255))
+    template_name: Mapped[Optional[str]] = mapped_column(String(255))
+    active: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    def __repr__(self) -> str:
+        return self.url_title
+
+
+class Ministry(BaseModel):
+
+    __tablename__ = 'ministries'
+    access_node_full_name = f"base.{__tablename__}"
+
+    name: Mapped[str] = mapped_column(String(255), unique=True)
+    short_description: Mapped[Optional[str]] = mapped_column(String(255))
+    description: Mapped[Optional[str]] = mapped_column(Text)
+
+    def __repr__(self) -> str:
+        return self.name
+
+
+class Preaching(BaseModel):
+
+    __tablename__ = 'preachings'
+    access_node_full_name = f"base.{__tablename__}"
+
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[Optional[str]] = mapped_column(Text)
+    video_url: Mapped[Optional[str]] = mapped_column(URLType)
+    thumbnail_url: Mapped[Optional[str]] = mapped_column(URLType)
+    preacher_id: Mapped[Optional[int]] = mapped_column(Integer, 
+            ForeignKey('users.id'))
+
+    def __repr__(self) -> str:
+        return self.title
+    
+
+class Event(BaseModel):
+
+    __tablename__ = 'events'
+    access_node_full_name = f"base.{__tablename__}"
+
+    name: Mapped[str] = mapped_column(String(255))
+    short_description: Mapped[Optional[str]] = mapped_column(String(255))
+    description: Mapped[Optional[str]] = mapped_column(Text)
+    venue: Mapped[Optional[str]] = mapped_column(String(255))
+    start_datetime: Mapped[datetime] = mapped_column(DateTime)
+    end_datetime: Mapped[Optional[datetime]] = mapped_column(DateTime)
+    repeat: Mapped[Optional[List[str]]] = mapped_column(ScalarListType(str))
+    repeat_on: Mapped[Optional[List[str]]] = mapped_column(ScalarListType(str))
+    include_time: Mapped[bool] = mapped_column(Boolean, default=True)
