0ca1119e88a5afe67862ef195935282184fd2213
Liel Onairam Tan <liel.onairam.tan@gmail.com>
2024-05-08 00:13:23 +0800

    Added documentation.

--- app/core/localization.py
+++ app/core/localization.py
@@ -9,23 +9,33 @@ import pytz
 
 
 def get_timezone() -> Optional[pytz.timezone]:
+    """Returns the local timezone set in user session."""
+
     if (tz := session.get('tz', None)):
         return pytz.timezone(tz)
     
 def set_timezone(tz: Union[pytz.timezone, str]):
+    """Sets the local timezone by user session."""
+
     if isinstance(tz, str):
         tz = pytz.timezone(tz)
 
     session['tz'] = tz.zone
 
 def utcnow() -> datetime:
+    """Similar to datetime.utcnow only having a timezone."""
+
     return datetime.now(pytz.timezone('UTC'))
 
 def now() -> datetime:
+    """Similar to datetime.now only having a timezone."""
+
     tz = get_timezone() or pytz.timezone('UTC')
     return datetime.now(tz)
 
 def to_user_timezone(dt: datetime) -> datetime:
+    """Converts the given datetime to the local timezone set in session."""
+
     if dt.tzinfo is None:
         dt = dt.replace(tzinfo=pytz.timezone('UTC'))
 
@@ -33,6 +43,8 @@ def to_user_timezone(dt: datetime) -> datetime:
     return tz.normalize(dt.astimezone(tz))
 
 def to_utc(dt: datetime) -> datetime:
+    """Converts the given datetime to UTC."""
+
     if dt.tzinfo is None:
         tz = get_timezone() or pytz.timezone('UTC')
         dt = tz.normalize(dt)
@@ -43,5 +55,6 @@ def render_datetime(
         dt: datetime,
         format: str = '%Y-%m-%d %H:%M:%S %Z%z',
     ) -> str:
+    """Uses to_user_timezone to convert the given datetime and formats it."""
     
     return to_user_timezone(dt).strftime(format)
